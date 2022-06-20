24da0fd0d002eb4ab326484a2e44a26c506f8e2a
jimmy <1024856723@qq.com>
2022-06-20 22:43:20 +0800

    agentmgr reqlogin

--- service/agentmgr/init.lua
+++ service/agentmgr/init.lua
@@ -1,3 +1,6 @@
+local skynet = require("skynet")
+local s = require("service")
+
 STATUS = {
     LOGIN = 2,
     GAME = 3,
@@ -14,4 +17,56 @@ function mgrPlayer()
         gate = nil,
     }
     return m
+end
+
+s.resp.reqlogin = function(source,playerId,node,gate)
+    local mplayer = players[playerId]
+    if mplayer and mplayer.status == STATUS.LOGOUT then
+        skynet.error("reqlogin fail,at status LOGOUT"..playerId)
+        return false
+    end
+    if mplayer and mplayer.status == STATUS.LOGIN then
+        skynet.error("reqlogin fail,at status LOGIN"..playerId)
+        return false
+    end
+    if mplayer then
+        local pnode = mplayer.node
+        local pagent = mplayer.agent
+        local pgate = mplayer.gate
+        mplayer.status = STATUS.LOGOUT
+        s.call(pnode,pagent,"kick")
+        s.send(pnode,pagent,"exit")
+        s.send(pnode,pgate,"send",playerId,{"kick","¶¥ÌæÏÂÏß"})
+        s.call(pnode,pgate,"kick",playerId)
+    end
+    local player = mgrPlayer()
+    player.playerId = playerId
+    player.node = node
+    player.gate = gate
+    player.agent = nil
+    player.status = STATUS.LOGIN
+    players[playerId] = player
+    local agent = s.call(node,"nodemgr","newservice","agent","agent",playerId)
+    player.agent = agent
+    player.status = STATUS.GAME
+    return true,agent
+end
+
+s.resp.reqkick = function(source,playerId,reason)
+    local mplayer = players[playerId]
+    if not mplayer then
+        return false
+    end
+    if mplayer.status ~= STATUS.GAME then
+        return false
+    end
+    local pnode = mplayer.node
+    local pagent = mplayer.agent
+    local pgate = mplayer.gate
+    mplayer.status = STATUS.LOGOUT
+    s.call(pnode,pagent,"kick")
+    s.send(pnode,pagent,"exit")
+    s.send(pnode,pgate,"kick",playerId)
+    players[playerId] = nil
+    return true
 end
