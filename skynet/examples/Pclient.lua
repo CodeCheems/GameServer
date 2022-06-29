727c620f80a247d17ded53481ef932fc56bd1b89
jimmy <1024856723@qq.com>
2022-06-29 23:29:52 +0800

    test json_pack

--- skynet/examples/Pclient.lua
+++ skynet/examples/Pclient.lua
@@ -5,8 +5,10 @@ local socket = require("client.socket")
 local fd = socket.connect("127.0.0.1",8888)
 socket.usleep(1*1000000)
 
-local bytes = string.pack(">Hc13",13,"login,101,134")
+local bytes = string.pack(">Hc13Hc4Hc2",13,"login,101,134",4,"work",4,"wo")
 socket.send(fd,bytes)
 
 socket.usleep(1*1000000)
+bytes = string.pack(">c2","rk")
+socket.send(fd,bytes)
 socket.close(fd)
