e57da412c1a5354f6826027923227b3223ae5093
jimmy <1024856723@qq.com>
2022-06-29 23:30:15 +0800

    test json_pack

--- service/Pmain.lua
+++ service/Pmain.lua
@@ -1,8 +1,23 @@
 local skynet = require("skynet")
 local socketdriver = require("skynet.socketdriver")
 local netpack = require("skynet.netpack")
-
+local cjson = require("cjson")
 local queue
+
+function json_pack(cmd,msg)
+    msg._cmd = cmd
+    local body = cjson.encode(msg)
+    local namelen = string.len(cmd)
+    local bodylen = string.len(body)
+    local len = namelen + bodylen + 2
+    local format = string.format("> i2 i2 c%d c%d",namelen,bodylen)
+    local buff = string.pack(format,len,namelen,cmd,body)
+    return buff
+end
+
+function json_unpack()
+
+end
 
 function socket_unpack(msg,sz)
     return netpack.filter(queue,msg,sz)
@@ -28,6 +43,8 @@ end
 function process_msg(fd,msg,sz)
     local str = netpack.tostring(msg,sz)
     skynet.error("recv from fd : "..fd.." str : "..str)
+    skynet.sleep(100)
+    skynet.error("finish fd : "..fd.." "..str)
 end
 
 function process_more()
@@ -55,12 +72,30 @@ function socket_dispatch(_,_,q,type,...)
 end
 
 skynet.start(function()
-    skynet.register_protocol({
+    --[[skynet.register_protocol({
         name = "socket",
         id = skynet.PTYPE_SOCKET,
         unpack = socket_unpack,
         dispatch = socket_dispatch,
     })
     local listenfd = socketdriver.listen("0.0.0.0",8888)
-    socketdriver.start(listenfd)
+    socketdriver.start(listenfd)]]
+    local msg = {
+        _cmd = "balllist",
+        balls = {
+            [1] = {id=102,x=10,y=20,size=1},
+            [2] = {id=103,x=10,y=20,size=2},
+        }
+    }
+    local buff = cjson.encode(msg)
+    print(buff)
+
+    buff = [[{"_cmd":"balllist","balls":[{"size":1,"x":10,"y":20,"id":102},{"size":2,"x":10,"y":20,"id":103}]}]]
+    local isok,ms = pcall(cjson.decode,buff)
+    if isok then
+        print(ms._cmd)
+        print(ms.balls[1].id)
+    else
+        print("error")
+    end
 end)
